e22c3f50dc50afdc6c8e5d2a2ed4d8439ac4937b
Debottam Kundu <debottam.kundu330@gmail.com>
2026-05-04 16:54:08 +0200

    single zeta action kernel priors

--- ibl_info/action_kernel_fits.py
+++ ibl_info/action_kernel_fits.py
@@ -6,72 +6,58 @@ import brainbox.io.one as bbone
 from ibl_info.pseudosession import get_requisite_eids
 from ibl_info.utils import check_config
 import pickle as pkl
+from brainwidemap import bwm_query, load_good_units, load_trials_and_mask, bwm_units
 
 config = check_config()
 
 
-def process_session(eid, one):
+def process_session(session_id):
 
     try:
 
-        one.load_object(eid, "trials")
-        sl = bbone.SessionLoader(one=one, eid=eid)
-        sl.load_trials()
+        one = ONE(
+            mode="local",
+        )
+        print(session_id)
+        trials, mask = load_trials_and_mask(
+            one,
+            session_id,
+            exclude_nochoice=True,  # True
+            exclude_unbiased=False,  # should include no-choice trials
+            min_rt=0.02,
+        )
 
         my_model = models.ActionKernel(
-            path_to_results="results_behavioral",
-            session_uuids=eid,
-            df_trials=sl.trials,
-            single_zeta=False,
+            path_to_results="results_behavioral_zeta",
+            session_uuids=session_id,
+            df_trials=trials,
+            single_zeta=True,  # should be True?
         )
 
         my_model.load_or_train(remove_old=False, adaptive=True)
 
         df_prior = my_model.predict_trials()
-        df_trials = sl.trials.join(df_prior, how="left")
-        return (eid, df_trials)
+        df_trials = trials.join(df_prior, how="left")
+        return (session_id, df_trials)
 
     except Exception as e:
-        print(f"Error processing {eid}: {e}")
-        return (eid, None)
+        print(f"Error processing {session_id}: {e}")
+        return (session_id, None)
 
 
 if __name__ == "__main__":
-
-    important_regions = [
-        "VISp",
-        "MOs",
-        "SSp-ul",
-        "ACAd",
-        "PL",
-        "CP",
-        "VPM",
-        "MG",
-        "LGd",
-        "ZI",
-        "SNr",
-        "MRN",
-        "SCm",
-        "PAG",
-        "APN",
-        "RN",
-        "PPN",
-        "PRNc",
-        "PRNr",
-        "GRN",
-        "IRN",
-        "PGRN",
-        "CUL4 5",
-        "SIM",
-        "IP",
-    ]
 
     one = ONE(
         base_url="https://openalyx.internationalbrainlab.org",
         username="intbrainlab",
         password="international",
     )
-    global_eid_list = get_requisite_eids(one, important_regions)
+    # global_eid_list = get_requisite_eids(one, important_regions)
+
+    bwm_df = bwm_query(one)
+    global_eid_list = bwm_df["eid"].unique()
+
+    workers = 32
 
     results_list = Parallel(n_jobs=-1)(
         delayed(process_session)(eid, one) for eid in global_eid_list
@@ -79,5 +65,5 @@ if __name__ == "__main__":
 
     big_dict = {eid: df for eid, df in results_list if df is not None}  # type: ignore
 
-    with open("./data/processed/all_eids_dict.pkl", "wb") as f:
+    with open("./data/processed/all_eids_dict_single_zeta_complete_bwm.pkl", "wb") as f:
         pkl.dump(big_dict, f)
